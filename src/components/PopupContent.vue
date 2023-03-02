78b7132b0510e5e9deb02aa682877518df6e8082
Zach1281 <zachary_romero@yahoo.com>
2023-03-01 21:00:55 -0800

    upload of final map

---
+++ src/components/PopupContent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="popup-content">
+      <h2>{{ title }}</h2>
+      <p>{{ description }}</p>
+      <button @click="closePopup">Close</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      title: String,
+      description: String,
+    },
+    methods: {
+      closePopup() {
+        this.$emit('close');
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .popup-content {
+    max-width: 200px;
+  }
+  </style>
+  
